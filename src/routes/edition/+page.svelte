7998972fb79a78abe350186f92763f5de73a812e
pythoncraft2 <pythoncraft2@gmail.com>
2023-11-28 14:56:58 +0100

    test snippet

--- src/routes/edition/+page.svelte
+++ src/routes/edition/+page.svelte
@@ -12,8 +12,6 @@
             GET(`/api/evenement/${id}`)
                 .then((v) => {
                     infos = v;
-                    // console.log(v);
-                    // dragDisabled = Device.isMobile || Device.isTablet;
                 })
                 .catch((e) => console.log(e));
         } else {
@@ -22,10 +20,96 @@
         console.log(id);
         
 	})
+	let { message = `it's great to see you!` } = $props();
 </script>
 
-{#if id}
-<h1>IIIIIIIIID</h1> 
-{:else}
-<h2>NO ID</h2>
-{/if}
+{#snippet liste_participants()}
+<ul style="border: 1px solid;">
+    <li>run</li>
+    <li>run</li>
+    <li>run</li>
+</ul>
+{/snippet}
+
+<h1>Sans voitures</h1>
+{@render liste_participants()}
+
+<h1>Voitures</h1>
+<button>Ajouter voiture</button>
+{#each {length: 3} as _, i}
+    <p>Voiture {i}</p>
+    {#each {length: 5} as _, x}
+        {@render liste_participants()}
+    {/each}
+{/each}
+
+
+
+
+
+
+
+
+
+
+<!-- <ZoneTrajets>
+    {#each infos.trajets.split("/") as typeTrajet}
+        <ZoneListes {typeTrajet}>
+            <ListeParticipants
+                slot="participants-sans-voiture"
+                items={infos.participants
+                    .filter((v) => v[`voiture_${typeTrajet}_id`] === null)
+                    .sort((a, b) => a.nom.localeCompare(b.nom))}
+                materiels={infos.materiels
+                    .filter((v) => v[`voiture_${typeTrajet}_id`] === null)
+                    .sort((a, b) => a.nom.localeCompare(b.nom))}
+                titre="Participants sans voiture ({infos.participants.filter(
+                    (v) => v[`voiture_${typeTrajet}_id`] === null,
+                ).length})"
+                titreClass="liste-participants"
+                {typeTrajet}
+                {dragDisabled}
+                bind:infos
+            />
+                <Bouton
+                    texte="Ajouter une voiture {typeTrajet} 🚘"
+                    slot="bouton-ajout-voiture"
+                    --bouton-largeur="auto"
+                    {visible}
+                    on:click={() =>
+                        toggleModal(AjoutVoiture, {
+                            typeTrajet,
+                            titreModal: `Ajouter voiture <span style='color: #7CC724'>${typeTrajet}</span>`,
+                        })}
+                />
+
+            <ListeVoitures slot="participants-avec-voiture">
+                {#each infos.voitures.filter((v) => v.trajets === typeTrajet) as voiture, index (voiture.id)}
+                    <ListeParticipants
+                        items={infos.voitures[index][`passagers_${typeTrajet}`]}
+                        materiels={infos.voitures[index][`materiels_${typeTrajet}`]}
+                        titre={voiture.nom}
+                        nb_places={voiture.nb_places}
+                        {index}
+                        {typeTrajet}
+                        {voiture}
+                        {toggleModal}
+                        {dragDisabled}
+                        bind:infos
+                    >
+                        <Bouton
+                            slot="bouton-ajout-passager"
+                            texte="Ajouter passager"
+                            --bouton-largeur="100%"
+                            {visible}
+                            on:click={() =>
+                                toggleModal(AjoutPassager, {
+                                    titreModal: `Ajouter passager dans<br><span style='color: #006699'>${voiture.nom}</span>`,
+                                })}
+                        />
+                    </ListeParticipants>
+                {/each}
+            </ListeVoitures>
+        </ZoneListes>
+    {/each}
+</ZoneTrajets> -->
