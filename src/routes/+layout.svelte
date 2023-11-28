1727c994842fd0d0c506de1d3d85089347d9d29a
pythoncraft2 <pythoncraft2@gmail.com>
2023-11-28 14:33:44 +0100

    test utilisation $effect et $state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,20 +7,5 @@
 </main>
 
 <style>
-	@import 'open-props/style';
-	@import 'open-props/normalize';
-	@import 'open-props/buttons';
-	@import '@fontsource-variable/manrope';
-
-	:global(html, body) {
-		height: 100%;
-	}
-
-	:global(body) {
-		/* display: grid; */
-		/* place-content: center; */
-		font-family: 'Manrope Variable', sans-serif;
-		color: hsl(220 10% 98%);
-		background-color: hsl(220 10% 8%);
-	}
+ 	@import "../lib/scss/main.scss";
 </style>
