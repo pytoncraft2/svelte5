bffdb38d20a5ef126a215df790e323dca7870d59
pythoncraft2 <pythoncraft2@gmail.com>
2023-11-29 23:38:53 +0100

    début réutilisation snippet

--- src/routes/edition/+page.svelte
+++ src/routes/edition/+page.svelte
@@ -8,6 +8,7 @@
     import BandeauInfo from "$lib/BandeauInfo.svelte";
     import TelechargementEtCheckbox from "$lib/disposition/TelechargementEtCheckbox/TelechargementEtCheckbox.svelte";
     import Liste from "../../lib/liste/Liste.svelte";
+    import ZoneListes from "../../lib/disposition/ZoneListes.svelte"
 
     let id = $state();
     let infos = $state({
@@ -62,10 +63,12 @@
 <TelechargementEtCheckbox bind:config {telechargement} />
 {#if infos}
     {#each infos.trajets.split("/") as typeTrajet}
-        <h1>Sans voitures {typeTrajet}</h1>
+    <ZoneListes {typeTrajet}>
+        {#snippet participants_sans_voiture()}
         <Liste items={infos.participants
                         .filter((v) => v[`voiture_${typeTrajet}_id`] === null)
                         .sort((a, b) => a.nom.localeCompare(b.nom))} />
+        {/snippet}
         <h3>Voitures {typeTrajet}</h3>
         <button onclick={() => toggleModal(AjoutVoiture, {
                             typeTrajet,
@@ -81,11 +84,127 @@
             <Liste items={infos.voitures[index][`materiels_${typeTrajet}`]} />
             <button>Ajouter Materiel</button>
         {/each}
+    </ZoneListes>
     {/each}
 {/if}
 
 <Modal bind:showModal {modalContent} {modalData} {infos} />
 
+<style lang="scss">
+    h2 {
+        text-align: center;
+    }
+
+        .bar-text {
+            display: flex;
+            align-items: center;
+            border: 1px solid #000;
+            padding: 10px;
+        }
+
+        .bar-text span {
+            flex-grow: 1;
+            text-align: center;
+            font-size: 18px;
+            font-weight: bold;
+        }
+
+        .bar-text::before,
+        .bar-text::after {
+            content: "";
+            flex-grow: 1;
+            border-top: 1px solid #000;
+            margin: 0 10px;
+        }
+
+    .groupe-liste {
+        display: flex;
+        position: relative;
+        padding-top: 1%;
+        gap: 1em;
+        @media screen and (max-width: 480px) {
+            gap: 0em;
+        }
+            // gap: 0.2em;
+    }
+
+    .groupe-boutons.ajouts {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 1em;
+    }
+
+    .zone-participant-sans-voiture {
+        // display: flex;
+        justify-content: center;
+        // align-items: center;
+        padding-bottom: 1em;
+        flex:1;
+    }
+
+    .liste {
+        background-color: white;
+        flex: 1;
+        position: -webkit-sticky;
+        position: -moz-sticky;
+        position: -o-sticky;
+        position: -ms-sticky;
+        position: sticky;
+        position: sticky;
+        top: 0;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        @media screen and (max-width: 480px) {
+            width: 100%;
+        }
+    }
+
+    .zone-bouton-voitures {
+        display: flex;
+        flex-direction: column;
+        flex: 3;
+        top: 0;
+        @media only screen and (max-width: 1490px) {
+            position: static;
+        }
+
+        @media only screen and (max-width: 768px) {
+            flex: 1;
+        }
+    }
+
+    .zone-voiture {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 1em;
+        @media only screen and (max-width: 768px) {
+            // margin-bottom: 50vh;
+            gap: 1em 0;
+            display: flex;
+            flex-direction: column;
+        }
+
+    }
+
+        :global(.zone-voiture section.places) {
+            flex-wrap: nowrap;
+            flex-direction: column;
+        }
+
+    #invisible-flex {
+        flex: 1;
+        @media only screen and (max-width: 1440px) {
+            display: none;
+        }        
+    }
+
+    // .zoneListes {
+    //     max-width: 540px;
+    // }
+
+</style>
 <!-- <ZoneTrajets>
     {#each infos.trajets.split("/") as typeTrajet}
         <ZoneListes {typeTrajet}>
