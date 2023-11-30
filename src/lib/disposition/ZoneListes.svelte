affa0bb067948f284e712d6082b4e5145056db29
pythoncraft2 <pythoncraft2@gmail.com>
2023-11-30 22:58:46 +0100

    suppression totale du style sur la page edition

--- src/lib/disposition/ZoneListes.svelte
+++ src/lib/disposition/ZoneListes.svelte
@@ -34,6 +34,53 @@
 </div>
 
 <style lang="scss">
+    .zoneListes {
+        display: flex;
+        gap: 1em;
+        justify-content: center;
+        flex-direction: var(--horizontale);
+        // @media screen and (max-width: 888px) {
+            // flex-direction: column;
+        // }
+    }
+
+    // :global(#invisible-flex) {
+    //     display: var(--invisible);
+    // }
+
+    // :global(.liste) {
+        // display: var(--liste) !important;
+        // width: auto !important;
+    // }
+
+    :global(.zoneListes) {
+        flex: 1 !important;
+        // max-width: none !important;
+    }
+
+    :global(.groupe-liste) {
+        flex-direction: var(--p-haut);
+    }
+
+    :global(.zone-participant-sans-voiture) {
+        max-width: var(--z-participant-sv);
+        display: var(--z-participant-display);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     h2 {
         text-align: center;
     }
